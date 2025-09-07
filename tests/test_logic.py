245a010dd60ff0946cb9711e51979725fc583e50
Shubhanshu Jha <52618335+ShubhanshuJha@users.noreply.github.com>
2025-09-07 22:48:29 +0530

    Updated test script

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -11,6 +11,11 @@ Covers:
 
 import pytest
 import json
+from os import path as os_path
+from sys import path as sys_path
+
+sys_path.append(os_path.realpath('../'))
+sys_path.append(os_path.realpath('./'))
 
 from pricing_logic import material_db, labor_calc, vat_rules
 import pricing_engine
